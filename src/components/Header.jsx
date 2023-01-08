bb883c07c777abaf7c2f7c64cd508439ba4c3e70
Sarah Salvatore <sarah.h.salvatore@gmail.com>
2023-01-08 12:01:24 -0500

    Create keyboard and styling

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -3,7 +3,7 @@ import React from "react";
 const Header = () => {
   return (
     <header>
-      <h1>Wordle</h1>
+      <h1>WORDLE</h1>
     </header>
   );
 };
--- src/components/Keyboard.jsx
+++ src/components/Keyboard.jsx
@@ -1,7 +1,38 @@
 import React from "react";
+import { keyboard } from "../data/keyboard";
 
 const Keyboard = () => {
-  return <div>Keyboard</div>;
+  return (
+    <div className="keyboard">
+      <div className="keyboard-row">
+        {keyboard[0].map((key) => {
+          return (
+            <div key={key} className="key">
+              {key}
+            </div>
+          );
+        })}
+      </div>
+      <div className="keyboard-row">
+        {keyboard[1].map((key) => {
+          return (
+            <div key={key} className="key">
+              {key}
+            </div>
+          );
+        })}
+      </div>
+      <div className="keyboard-row">
+        {keyboard[2].map((key) => {
+          return (
+            <div key={key} className="key">
+              {key}
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Keyboard;
